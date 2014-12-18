5ee6404a27f4a625530f4300b59363ea8b4aeacf
Luke <luke@Lukes-Mac-Pro.local>
2014-12-18 13:38:50 -0800

    Added class

---
+++ academicus/Level.h
@@ -0,0 +1,13 @@
+//
+//  Level.h
+//  academicus
+//
+//  Created by Luke on 18/12/2014.
+//  Copyright (c) 2014 sheffield. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Level : NSObject
+
+@end
---
+++ academicus/Level.m
@@ -0,0 +1,13 @@
+//
+//  Level.m
+//  academicus
+//
+//  Created by Luke on 18/12/2014.
+//  Copyright (c) 2014 sheffield. All rights reserved.
+//
+
+#import "Level.h"
+
+@implementation Level
+
+@end
