e42b3b974bb9fb219bfc4f4f3dbc958ad33a3dcc
Ben <ben@Bens-Mac-Pro.local>
2014-12-18 21:03:07 +0000

    Initial Commit

---
+++ academicus/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  academicus
+//
+//  Created by Ben on 18/12/2014.
+//  Copyright (c) 2014 sheffield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ academicus/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  academicus
+//
+//  Created by Ben on 18/12/2014.
+//  Copyright (c) 2014 sheffield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
