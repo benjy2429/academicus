5c0683ef2ce094e0f3f0df42812180ac7ed37ed2
Ben <ben@Bens-Mac-Pro.local>
2014-12-18 22:15:30 +0000

    added classes for portfolio, achievements, and work experiences

---
+++ academicus/WorkExperience.h
@@ -0,0 +1,22 @@
+//
+//  WorkExperience.h
+//  academicus
+//
+//  Created by Ben on 18/12/2014.
+//  Copyright (c) 2014 sheffield. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WorkExperience : NSObject
+
+@property (strong) NSString *companyName;
+@property (strong) NSString *companyAddress;
+@property (strong) NSString *jobTitle;
+@property (strong) NSDate *startDate;
+@property (strong) NSDate *endDate;
+@property (strong) NSString *refereeName;
+@property (strong) NSString *refereeEmail;
+@property (strong) NSString *description;
+
+@end
