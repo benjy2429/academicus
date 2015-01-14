bff0833d2e450c0afe659d02c942ebb54cbf4b6b
Ben <ben@Bens-Mac-Pro.local>
2015-01-14 18:32:43 +0000

    added automatic 3 week notifications + cool bounce throbbing animation on logo

--- academicus/AboutTableViewController.m
+++ academicus/AboutTableViewController.m
@@ -24,10 +24,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-
-    
     // Override the height of the table view header
-    self.tableView.tableHeaderView.frame = CGRectMake(0, 0, 0, 100);
+    self.tableView.tableHeaderView.frame = CGRectMake(0, 0, 0, 110);
+    
+    // Set the logo wrapper to a circle shape with an app tint background colour
+    UIView *logoWrapper = (UIView *)[self.tableView.tableHeaderView viewWithTag:700];
+    logoWrapper.backgroundColor = APP_TINT_COLOUR;
+    logoWrapper.layer.masksToBounds = YES;
+    logoWrapper.layer.cornerRadius = 45;
+    
+    // Add a gesture recogniser to respond to a tap
+    UITapGestureRecognizer *tapGestureRecogniser = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(logoTapped)];
+    tapGestureRecogniser.numberOfTapsRequired = 1;
+    [logoWrapper addGestureRecognizer:tapGestureRecogniser];
+}
+
+
+- (void)logoTapped
+{
+    // Get the logo wrapper view
+    UIView *logoWrapper = (UIView *)[self.tableView.tableHeaderView viewWithTag:700];
+    
+    // Create a keyframe animation to produce a bounce effect when tapped
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+    animation.duration = 1.2f;
+    animation.values = @[[NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0f, 1.0f, 1.0f)],
+                         [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.8f, 0.8f, 1.0f)],
+                         [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.2f, 1.2f, 1.0f)],
+                         [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.9f, 0.9f, 1.0f)],
+                         [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.1f, 1.1f, 1.0f)],
+                         [NSValue valueWithCATransform3D:CATransform3DMakeScale(1.0f, 1.0f, 1.0f)]];
+    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+    animation.removedOnCompletion = NO;
+    animation.autoreverses = NO;
+    
+    // Add the animation to the layer
+    [logoWrapper.layer addAnimation:animation forKey: @"transform"];
+    
 }
 
 
